29174367fc6e489e4ca0f378a402aeab3f96a4fb
ClementDustanSiaffu <clementsiaffu95@gmail.com>
2020-04-10 01:26:27 +0300

    8 th changes

--- components/about.js
+++ components/about.js
@@ -21,11 +21,8 @@ function About (){
              <Container>
                  <div className = "left4" data-aos="flip-up">
                         <h1 className = "left5">Profile</h1>
-                        <p className = "left6">I'm creative JavaScript web developer</p>
-                        <p className = "left43">
-                    "We gotta start teaching our children that they can be all they want to be 
-                    there are so much to life than just poverty" -Tupac Amaru Shakur
-                        </p>
+                        <p className = "left6">"Knowlegde is a treasure,but practise is the key to it."-IBN KHALDOUN AL MUQADDIMA</p>
+                      
                   
     
           </div>
@@ -42,27 +39,9 @@ function About (){
                     <Col xs = "auto" >
                         <div  >
 
-     <h1 className = "left42">Meet Clement Dunstan Siaffu</h1>  
-     <p className= "left8" >
-    Clement Dunstan Siaffu is the degree holder in the Electronics and Telecommunication Engineering.His exposure
-    to the world of programming started in 2016 when he was learning object oriented programming and 
-    Data stucture programming as ones of his class modules and things did not go well for him at that time,<span className="left44">
-    "In the magical universe there are no coincidences and there are no accidents.Nothing happens unless someone wills
-    it to happen."- WILLIAM S. BURROUGHS</span>,untill 2017 where things got better for him after getting support from his friends,Clement says that
-    <span className = "left44">"I could have never reached  this far without that support,because though that support
-    I was able to discover alot of things about programming which helped me for self-teaching,so I am thankfully
-    for the breath God gave me and am thankfully for the support I got"</span>,he finished by saying<span className="left44">"A thankful heart hath a continual feast."-W.J CAMEROON</span> 
-    .Through self-teaching he was able to learn alot of things concerning programming and that made him to be passionate about web development
-    .<span className = "left44">"Knowlegde is a treasure,
-    but practise is the key to it."-IBN KHALDOUN AL MUQADDIMA</span>,Clement believes
-    that practising programming is an endeless process which require learning new things everyday and that why
-    he loves reading books,watching tutorials,sharing ideas,team working,working on different programming projects
-    and learning things from other developers and programmers.
-
-    <h1 className = "left9">
-    "The best education you can get is investing in yourself and that does not mean college or university"
-       -Warren Buffet</h1>
-        
+      
+            <p className= "left8" >
+             Hello,My name is Clement Dunstan Siaffu,I am creative JavaScript web developer
             </p>
             </div>
 
--- components/home.js
+++ components/home.js
@@ -24,10 +24,9 @@ function Home(){
     <div className = "left2" > 
     <Typical 
         steps={[
-            'Converting something from my mind to something that I can present to the real world make me happier "Creativity is the intelligence having fun"-Albert Einstein',1000,
+            '"The best education you can get is investing yourself and that does not mean college or university"-Warren Buffet',1000,
             '"If you take any activity,any art,any discipline,any skill-take it and push it as far as it will go,push it beyond where it has ever been before,push it to the wildest edges of edges,then you force it into the realm of magic"-Tom Robbins',1000,
-           `"We all die.The goal isn't to live forever,the goal is to create something that will" - Chunk Palahniuk `,1000,
-           '"Do the best you can untill you know better.Then when you know better,do better"- Maya Angelou'
+           '"Do the best you can untill you know better.Then when you know better,do better"- Maya Angelou',1000,
         ]}
         loop={Infinity}
         wrapper="h3" 
